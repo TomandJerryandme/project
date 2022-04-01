18e1bbe3e7836d41200453c719635962614d681d
liuxuan02 <liuxuan02@inspur.com>
2022-04-01 11:24:21 +0800

    gitignore update

--- Project/Python/video_download_test.py
+++ Project/Python/video_download_test.py
@@ -6,8 +6,14 @@ import requests
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
                                     " (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36",
             "Connection": "keep-alive"}
-url = r'https://haokan.baidu.com/v?pd=wisenatural&vid=1008580542103423667'
-r = requests.get(url, headers=header, timeout=15)
-with open(r"D:\back\test_video.mp4", "wb") as f:
-    f.write(r.content)
-f.close()
+# url = r'https://haokan.baidu.com/v?pd=wisenatural&vid=1008580542103423667'
+# r = requests.get(url, headers=header, timeout=15)
+# with open(r"D:\back\test_video.mp4", "wb") as f:
+#     f.write(r.content)
+# f.close()
+
+
+# bili_url = 'https://www.bilibili.com'
+# result = requests.get(bili_url)
+# print(result.status_code)
+# print(result.content.decode(encoding='utf-8'))
